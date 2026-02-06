fc658d4925df2dabc7a84ce9de578366cd69d002
ssbaj <ssb9018@ajou.ac.kr>
2026-02-06 10:01:32 +0900

    first commit

---
+++ R/e_dfmvclass.R
@@ -0,0 +1,20 @@
+e_dfmvclass<-function(explaining=0){
+
+  if(explaining==0) {
+  cat("  ", '\n')
+  cat("\033[1;32m##  Dataset ---- \033[0m", '\n')
+  cat("  arrest", '\n')
+  cat("  coffee", '\n')
+  cat("  heywood", '\n')
+  cat("  KR_authority", '\n')
+  cat("  KR_senior_center", '\n')
+  cat("  OMKthesis", '\n')
+  cat("  Path_Ingram", '\n')
+  cat("  smartphone2018", '\n')
+  cat("  YS_authority", '\n')
+  cat("  YS_commitment", '\n')
+  cat("  YS_jobsatisfaction", '\n')
+  cat("  YS_jobsatisfaction2", '\n')
+  cat("  ", '\n')
+  } }
+  
